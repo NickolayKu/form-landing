b9ddeac8ac76a8d00b00788f6d4fb0b4cb781411
NickolayKu <nickolay135000@yandex.ru>
2024-05-29 00:20:23 +0300

    Added other projects block

--- src/components/OtherProjects/OtherProjects.jsx
+++ src/components/OtherProjects/OtherProjects.jsx
@@ -1,22 +1,28 @@
-import cls from "./HomeProjects.module.scss";
+/* eslint-disable react/prop-types */
+import cls from "./OtherProjects.module.scss";
 import ProjectCard from "../ProjectCard/ProjectCard";
+import ScrollAnimation from "react-animate-on-scroll";
 
-export default function HomeProjects() {
+export default function OtherProjects(props) {
+  const { projectName = null } = props;
 
   const projectsCards = [
     {
+      name: 'topgun',
       image: 'c7a96012faf046e0238087b69a89a0c5-min.jpg',
       title: 'topgun - сеть барбершопов',
       tags: ['UX/UI', 'Разработка'],
       link: '/topgun',
     },
     {
+      name: 'colizeum',
       image: 'fb89c4046b6ae08e3df0fa1a71d9ef11-min.jpg',
       title: 'colizeum - киберcпортивные арены',
       tags: ['SMM', 'Увеличение трафика'],
       link: '/colizeum',
     },
     {
+      name: 'magma',
       image: '6bcdac3d5991ba7fd927cf4b7e235f9a-min.jpg',
       title: 'magma - архитектурное бюро',
       tags: ['Брендинг', 'UX/UI', 'Разработка'],
@@ -26,9 +32,18 @@ export default function HomeProjects() {
 
   return (
       <div className="container">
-        <div className={cls.homeProjects}>
+        <ScrollAnimation animateIn="fadeInUp" duration={0.6} animateOnce={true}>
+          <h1 className={cls.h1}>
+            Другие проекты
+            <div className={cls.line}></div>
+          </h1>
+        </ScrollAnimation>
+
+        <div className={cls.otherProjects}>
           {projectsCards.map((item, index) => {
-            return <ProjectCard key={index} image={item.image} title={item.title} tags={item.tags} link={'/projects' + item.link}/>
+            if(item.name !== projectName){
+              return <ProjectCard key={index} image={item.image} title={item.title} tags={item.tags} link={'/projects' + item.link}/>
+            }
           })}
         </div>
       </div>
--- src/pages/PortfolioColizeum/PortfolioColizeum.jsx
+++ src/pages/PortfolioColizeum/PortfolioColizeum.jsx
@@ -2,100 +2,41 @@ import { useMetaTags } from "react-metatags-hook";
 import cls from "./PortfolioColizeum.module.scss";
 import PortfolioTitle from "../../components/PortfolioTitle/PortfolioTitle";
 import PortfolioTopgunContent from "../../components/PortfolioTopgunContent/PortfolioTopgunContent";
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useState } from "react";
+import OtherProjects from "../../components/OtherProjects/OtherProjects";
+import Footer from "../../components/Footer/Footer";
 
 export default function PortfolioColizeum() {
-  const location = useLocation();
-  const navigate = useNavigate();
 
-  const projectName = location.pathname.replace(/^\/+/g, '');
+  const projectData = {
+    name: 'colizeum',
+    title: 'colizeum - Cеть киберспортивных арен',
+    text: 'Colizeum - крупнейшая сеть киберспортивных клубов в мире и самый известный бренд среди геймеров. Всего открыто более 430 клубов Colizeum в 19 странах.',
+    tags: ['Разработка социальных сетей / увеличение трафика'],
+  };
 
   useMetaTags({
-    title: `FORM - ${projectName.toUpperCase()}`
+    title: `FORM - ${projectData.name.toUpperCase()}`
   });
-
-  const projects = [
-    {
-      name: 'topgun',
-      title: 'topgun - международная сеть барбершопов',
-      text: 'TOPGUN — международная сеть барбершопов, насчитывающая более 300 активных филиалов с общим оборотом 3,1 млрд р. Перед нами стояла задача разработать редизайн текущего визуала сайта, сохранив узнаваемость и характер бренда.',
-      tags: ['UX/UI', 'Разработка'],
-    },
-    {
-      name: 'colizeum',
-      title: 'colizeum - Cеть киберспортивных арен',
-      text: 'Colizeum - крупнейшая сеть киберспортивных клубов в мире и самый известный бренд среди геймеров. Всего открыто более 430 клубов Colizeum в 19 странах.',
-      tags: ['Разработка социальных сетей / увеличение трафика'],
-    },
-    {
-      name: 'magma',
-      title: 'MAGMA - архитектурное бюро',
-      text: 'Magma Architecture – московское архитектурное бюро, ориентированное на изучение поэтического потенциала архитектуры и совершенство дизайна.',
-      tags: ['Брендинг', 'UX/UI', 'Разработка'],
-    },
-  ];
-
-  const projectIndex = projects.findIndex(project => project.name === projectName);
-
-  const getNextProjectIndex = (currentIndex) => {
-    let nextIndex = null;
-    if(currentIndex + 1 >= projects.length){
-      nextIndex = 0;
-    } else {
-      nextIndex = currentIndex + 1;
-    }
-    
-    return nextIndex;
-  }
-
-  const nextProjectIndex = getNextProjectIndex(projectIndex);
-  
-  const [isMoreHeight, setIsMoreHeight] = useState(false);
-
-  const handleGoToNextPage = () => {
-    setIsMoreHeight(true);
-
-    setTimeout(() => {
-      window.scrollTo({ left: 0, top: document.body.scrollHeight + 500, behavior: "smooth" });
-    }, 50)
-
-    setTimeout(() => {
-      const event = new CustomEvent('onBeforeNextProject');
-      window.dispatchEvent(event);
-    }, 425)
-
-    setTimeout(() => {
-      navigate(`/${projects[nextProjectIndex].name}`);
-    }, 700)
-  }
   
   return (
     <>
-      <div className={cls.projectPageBlock}>
-        <section>
-          <PortfolioTitle title={projects[0].title} text={projects[0].text} tags={projects[0].tags}/>
-        </section>
-        <div className={cls.projectContentBlock}>
-          <section>
-            <PortfolioTopgunContent />
-          </section>
-          <section className="fullWidthSection">
-            <img src="/images/topgun-3-min.jpg" className={cls.fullWidth} />
-          </section>
-        
-          <section>
-            <div className={cls.nextProjectButton} onClick={() => handleGoToNextPage()}>
-              Следующий проект
-            </div>
-          </section>
-          <section>
-            <div className={[cls.nextPageBlock, isMoreHeight && cls.moreHeight].join(' ')} onClick={() => handleGoToNextPage()}>
-              <PortfolioTitle title={projects[nextProjectIndex].title} text={projects[nextProjectIndex].text} tags={projects[nextProjectIndex].tags}/>
-            </div>
-          </section>
-        </div>
-      </div>
+      <section>
+        <PortfolioTitle
+          title={projectData.title}
+          text={projectData.text}
+          tags={projectData.tags}
+        />
+      </section>
+      <section>
+        <PortfolioTopgunContent />
+      </section>
+      <section className="fullWidthSection">
+        <img src="/images/topgun-3-min.jpg" className={cls.fullWidth} />
+      </section>
+      <section>
+        <OtherProjects projectName={projectData.name} />
+      </section>
+      <Footer />
     </>
   );
 }
